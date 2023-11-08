b642b508ee19a59572afbbe2738593241b9b0593
awewsomegamer <awewsomegamer@gmail.com>
2023-11-08 13:10:33 -0800

    Added compile commands

---
+++ fib.s
@@ -0,0 +1,46 @@
+; Compile Command		
+; nasm -f elf64 -o fib.o fib.s		
+; gcc -static fib.o -o fib.out		
+; ./fib.out		
+		
+%define FIBONACCI_COUNT 35		
+		
+global main		
+extern printf		
+section .text		
+main:	push rbp	; Save stack base
+	mov rbp, rsp	; New stack frame
+		
+	mov rcx, 0	; Zero state
+	mov rdx, 0	; Zero counter
+loop:	mov rax, [n1]	; Get the first number
+	mov rbx, [n2]	; Get the second number
+	add rax, rbx	; Add them
+		
+	not rcx	; Test to see if RCX is zero
+	cmp rcx, 0	; Compare RCX to zero
+	jne latter	; If it is not zero jump over
+	mov [n1], rax	; Sum goes into n1
+	jmp end	; Jump over
+latter:	mov [n2], rax	; Sum goes into n2 
+end:	inc rdx	; Increment RDX counter
+	cmp rdx, FIBONACCI_COUNT	; Reached 16 iterations??
+	jle loop	; No - loop
+		
+	mov rdi, string	; Set format
+	mov rsi, rax	; Set fibonacci number
+	mov rdx, FIBONACCI_COUNT	
+	call printf	; Print
+		
+	xor rax, rax	; Zero return value
+		
+	mov rsp, rbp	; Restore stack pointer 
+	pop rbp	; Restore stack base
+		
+	ret	
+		
+section .data		
+		
+n1:	dq 0	; First number
+n2:	dq 1	; Second number
+string:	db "%d is at %d in the fibonacci sequence", 0xA, 0x0	; String format
